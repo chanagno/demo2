f29da80266bc93ec70e47f68f9fa7deb0033d80f
chanagno <47986877+chanagno@users.noreply.github.com>
2020-02-13 12:24:47 +0200

    Add files via upload

---
+++ spring-rest-hello-world/src/test/java/com/mkyong/AuthorRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.mkyong;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AuthorRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
